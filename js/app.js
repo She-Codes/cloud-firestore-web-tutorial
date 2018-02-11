8e08e7a0a7225e38dc140843b2fc0e4266d26dbb
She-Codes <n.y.shaw@gmail.com>
2018-02-10 21:36:05 -0500

    adding firebase auth to app

--- js/app.js
+++ js/app.js
@@ -7,48 +7,6 @@ let config = {
   storageBucket: "",
   messagingSenderId: "1001195068664"
 };
+
 firebase.initializeApp(config);
 
-let firestore = firebase.firestore();
-
-let docRef = firestore.doc('samples/sandwichData');
-let outputHeader = document.querySelector('#hotDogOutput');
-let inputTextField = document.querySelector('#latestHotDogStatus');
-let saveButton = document.querySelector('#saveButton');
-let loadButton = document.querySelector('#loadButton');
-
-saveButton.addEventListener('click', function() {
-  let textToSave = inputTextField.value;
-  console.log(`I am going to save ${textToSave} to Firestore`);
-  docRef.set({
-    hotDogStatus: textToSave
-  }).then(function() {
-    console.log('Status saved!');
-  }).catch(function(error) {
-    console.log(`Got an error: ${error}`);
-  });
-});
-
-loadButton.addEventListener('click', function() {
-  let myData;
-  docRef.get().then(function(doc) {
-    if ( doc && doc.exists ) {
-      myData = doc.data();
-      outputHeader.innerText = `Hot dog status: ${myData.hotDogStatus}`;
-    }
-  }).catch(function(error) {
-    console.log(`Got an error: ${error}`);
-  });
-});
-
-let getRealTimeUpdates = function() {
-  docRef.onSnapshot(function(doc) {
-    let myData;
-    if ( doc && doc.exists ) {
-      myData = doc.data();
-      outputHeader.innerText = `Hot dog status: ${myData.hotDogStatus}`;
-    }
-  });
-}
-
-getRealTimeUpdates();
---
+++ js/log-in.js
@@ -0,0 +1,79 @@
+// FirebaseUI config.
+let uiConfig = {
+  signInSuccessUrl: 'logged-in.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>'
+};
+
+let auth = firebase.auth();
+// Initialize the FirebaseUI Widget using Firebase.
+let ui = new firebaseui.auth.AuthUI(auth);
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+// if (ui.isPendingRedirect()) {
+//   ui.start('#firebaseui-auth-container', uiConfig);
+// }
+
+initApp = function() {
+  auth.onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      var phoneNumber = user.phoneNumber;
+      var providerData = user.providerData;
+      user.getIdToken().then(function(accessToken) {
+        document.getElementById('sign-in-status').textContent = 'Signed in';
+        document.getElementById('sign-in').innerHTML = `<div>
+      <button type="button" id="sign-out">sign out</button>
+    </div>`;
+        document.getElementById('account-details').textContent = JSON.stringify({
+          displayName: displayName,
+          email: email,
+          emailVerified: emailVerified,
+          phoneNumber: phoneNumber,
+          photoURL: photoURL,
+          uid: uid,
+          accessToken: accessToken,
+          providerData: providerData
+        }, null, '  ');
+      });
+    } else {
+      // User is signed out.
+      document.getElementById('sign-in-status').textContent = 'Signed out';
+      document.getElementById('sign-in').textContent = 'Sign in';
+      document.getElementById('account-details').textContent = 'null';
+    }
+  }, function(error) {
+    console.log(error);
+  });
+};
+
+window.addEventListener('load', function() {
+  initApp();
+});
+
+document.addEventListener('click', function(e) {
+  if ( e.target.id === 'sign-out' ) {
+    auth.signOut().then(function() {
+      document.location.assign(`${window.location.protocol}//${window.location.host}/index.html`);
+    }).catch(function(error) {
+      console.log(`sign out error: ${error}`);
+    });
+  }
+});
+
